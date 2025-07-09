53340802665dcf07080fd346b4061d3bc309717f
Tran The Lam <32351438+tran-the-lam@users.noreply.github.com>
2025-07-09 20:33:18 +0700

    Update gtag.ts

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -10,7 +10,7 @@ export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID; // T
 // Gửi page view đến Google Analytics
 export const pageview = (url: string) => {
     // console.log('Pageview:', url); // Log the pageview URL for debugging
-    // console.log('GA_MEASUREMENT_ID:', GA_MEASUREMENT_ID); // Log the GA_MEASUREMENT_ID for debugging
+    console.log('GA_MEASUREMENT_ID:', GA_MEASUREMENT_ID); // Log the GA_MEASUREMENT_ID for debugging
     if (!GA_MEASUREMENT_ID || typeof window === 'undefined') return;
     window.gtag('config', GA_MEASUREMENT_ID, {
         page_path: url,
